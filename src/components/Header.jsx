39cbcf70cc408246e6200dd05197641b6a823fe8
Duong2k1 <19110009@student.hcmute.edu.vn>
2022-11-24 20:47:49 +0700

    Update fix UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -45,7 +45,7 @@ const Header = () => {
 
     return (
         <div className={classes}>
-            <Row className='d-flex justify-content-between align-items-center flex-wrap mx-0 py-2' style={{background: '#ffffff'}}>
+            <Row className='d-flex justify-content-between align-items-center flex-wrap mx-0 py-2' style={{ background: '#ffffff' }}>
                 <Col xl={3} className='d-flex justify-content-center align-items-center'>
                     <LinkContainer to='/'>
                         <Navbar.Brand className='text-success'>
@@ -54,13 +54,18 @@ const Header = () => {
                         </Navbar.Brand>
                     </LinkContainer>
                 </Col>
-                <Col xl={6} style={{margin: '0 auto', padding: 'auto', height: 'auto'}} className='py-3'>
+                <Col xl={6} style={{ margin: '0 auto', padding: 'auto', height: 'auto' }} className='py-3'>
                     <Search />
                 </Col>
                 <Col xl={3} className='d-flex justify-content-center align-items-center'>
                     <LinkContainer style={{ color: 'white' }} to='/cart' className='px-3'>
                         <Nav.Link className='text-success'>
-                            <i style={{ fontSize: '30px' }} className='fas fa-shopping-cart'></i> <strong style={{ fontSize: '20px' }}>({cartItems.length})</strong>
+                            <i style={{ fontSize: '30px' }} className='fas fa-shopping-cart'></i> <strong style={
+                                { fontSize: '20px' }}>
+                                {
+                                    cartItems.length > 0 && `(${cartItems.length})`
+                                }
+                            </strong>
                         </Nav.Link>
                     </LinkContainer>
                     {userInfo ? (
